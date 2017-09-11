a34b83b4f3af07902322d6150b28e9940e957e07
Win7 <Win7@Win7-PC>
2017-09-11 20:07:28 +0200

    Created First Swing (GUI) Program

--- src/Program.java
+++ src/Program.java
@@ -3,13 +3,41 @@
  * @author Jasmin Adilovic
  *
  */
+
+
+import javax.swing.JOptionPane;
+
+
 public class Program {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String firstNumber = "";
+		String secondNumber = "";
+		
+		try {
+			//obtain user input from JOptionPane input dialogs
+			firstNumber = JOptionPane.showInputDialog("enter first integer");
+			System.out.println(firstNumber);
+			secondNumber = JOptionPane.showInputDialog("Enter second integer");
+			System.out.println(secondNumber);
+			
+			// convert String inputs to int values for use in a calculation
+			int number1 = Integer.parseInt(firstNumber);
+			int number2 = Integer.parseInt(secondNumber);
+			
+			int sum = number1 + number2;
+			
+			// display result in a JOptionPane message dialog
+	JOptionPane.showMessageDialog(null, "The sum is " + sum, "Sum of Two Integers", JOptionPane.PLAIN_MESSAGE);
+			
+			} catch (Exception e){
+				JOptionPane.showMessageDialog(null, "Incorect values entered", "Entry Error", JOptionPane.ERROR_MESSAGE);
+				System.out.println(e);
+		}
+		
 
 	}
 
